5cd61390b998e67422e0f33d7cadadc1a8de26bb
adarsh0223 <adarsh2chaudhary2@gmail.com>
2020-04-27 14:46:42 +0530

    change2

--- app/src/main/java/com/example/test3/MainActivity.java
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//lets makes changes here
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openMenu();
+    }
+
+    private void openMenu() {
     }
 }
